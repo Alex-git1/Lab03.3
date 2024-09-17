778a53a463cd4c0ce452b5c8f12167a39193429a
unknown <mrcricoarnold@gmail.com>
2024-09-17 16:52:35 +0300

    first commit

---
+++ Lab03.3/Lab03.3.cpp
@@ -0,0 +1,31 @@
+// Lab_03_3.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 13
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	if (x <= R) {
+		y = R - 3;
+	}
+	else if (0 < x && x <= R) {
+		y = R + sqrt(R * R - x * x);
+	}
+	else if (R <= x && x <= 6) {
+		y = R - ((R * x) / 6);
+	}
+	else if (x > 6) {
+		y = x + 6;
+	}
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
